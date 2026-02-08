271d1cf259dfd41e3ee3066e97e03c6a9a020fe0
abishekraja775 <abishekraja775@gmail.com>
2026-02-08 09:03:49 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,14 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+def preprocess_data(normal_data, test_data, n_features=2):
+    scaler = StandardScaler()
+    normal_scaled = scaler.fit_transform(normal_data)
+    test_scaled = scaler.transform(test_data)
+
+    pca = PCA(n_components=n_features)
+    normal_reduced = pca.fit_transform(normal_scaled)
+    test_reduced = pca.transform(test_scaled)
+
+    return normal_reduced, test_reduced
 
