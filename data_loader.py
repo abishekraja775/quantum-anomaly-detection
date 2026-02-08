576f62fe9bf0041cae13c406f01d318842d1e9ce
abishekraja775 <abishekraja775@gmail.com>
2026-02-08 09:03:07 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+def load_creditcard_data(path):
+    df = pd.read_csv(path)
+
+    normal = df[df["Class"] == 0].drop(columns=["Class"])
+    anomaly = df[df["Class"] == 1].drop(columns=["Class"])
+
+    return normal, anomaly
 
